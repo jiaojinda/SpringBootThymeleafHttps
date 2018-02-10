27405b29e6056ae9d0a89027732be500fe06406d
Administrator <jiaojinda@163.COM>
2018-02-10 09:55:24 +0800

    SpringBootThymeleafHttps upload

---
+++ src/main/java/org/jiao/CustomServletContainer.java
@@ -0,0 +1,22 @@
+package org.jiao;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Administrator on 2018/2/9.
+ */
+@Component
+public class CustomServletContainer implements EmbeddedServletContainerCustomizer {
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(8443);
+        container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,"/404.html"));
+        container.setSessionTimeout(10, TimeUnit.MINUTES);
+    }
+}
